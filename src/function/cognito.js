64291eea65ea21b06592058a9ccf784b3c8dc150
yozakura <matchnokaori@gmail.com>
2022-12-31 14:51:53 +0900

    added auth function

--- src/function/cognito.js
+++ src/function/cognito.js
@@ -8,6 +8,7 @@ export const setValue = [
   }
 ]
 
+// signup
 export async function signUp (username, password, nickname, profile) {
   try {
     const user = await Auth.signUp({
@@ -29,3 +30,40 @@ export async function signUp (username, password, nickname, profile) {
     return false;
   }
 }
+
+// resend code
+export async function resendCode (username) {
+  try {
+    await Auth.resendSignUp(username);
+    console.log("success resend code");
+    return true;
+  } catch (err) {
+    console.error("resend code error...", err);
+    return false;
+  }
+}
+
+// confirm code 
+export async function confirmCode (username, code) {
+  try {
+    await Auth.confirmSignUp(username, code);
+    console.log("confirm success");
+    return true;
+  } catch (err) {
+    console.log("error confirm code", err);
+    return false;
+  }
+}
+
+// sign in
+export async function signIn (username, password) {
+  try {
+    const user = await Auth.signIn(username, password);
+    console.log("success signin", user);
+    return true;
+  } catch (err) {
+    console.error("signin error...", err);
+    return false;
+  }
+}
+
