2320dbd5b3dd3d3c8550c4a531f9a7b621e77d39
yozakura <matchnokaori@gmail.com>
2023-01-08 21:47:32 +0900

    category image layout fix

--- src/function/s3/getCategoryImage.js
+++ src/function/s3/getCategoryImage.js
@@ -1,4 +1,5 @@
-import { GetObjectAclCommand, GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import DOMPurify from "dompurify";
 
 export async function getS3CategoryImage () {
   const REGION = process.env.NEXT_PUBLIC_COGNITO_REGION;
@@ -6,7 +7,6 @@ export async function getS3CategoryImage () {
     accessKeyId: process.env.NEXT_PUBLIC_ARTICLE_ACCESS_KEY_ID,
     secretAccessKey: process.env.NEXT_PUBLIC_ARTICLE_SECRET_KEY,
   }
-  console.log("情報", CREDENTIALS);
 
   const s3Client = new S3Client({ 
     region: REGION,
@@ -18,7 +18,6 @@ export async function getS3CategoryImage () {
     Bucket: process.env.NEXT_PUBLIC_ARTICLE_BUCKET,
     Key: "category-image/sass.svg",
   };
-  console.log("パラムス", );
 
   try {
     // get data
--- src/pages/articles/[id].jsx
+++ src/pages/articles/[id].jsx
@@ -3,6 +3,7 @@ import { getArticleId } from "@/function/axios";
 import { getS3CategoryImage } from "@/function/s3/getCategoryImage";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { CategoryImageArticle } from "@/components/articles/CategoryImage";
 
 export default function ArticleId () {
   const router = useRouter();
@@ -16,24 +17,22 @@ export default function ArticleId () {
 
   // 記事取得
   async function getArticle() {
-    console.log("呼ばれるか？", router.query.id);
     const response = await getArticleId(router.query.id);
     setArticleIds(response);
-    console.log("ブラウザ側レスポンス", response);
   }
   // 画像取得
   async function getImage() {
     const result = await getS3CategoryImage();
     // console.log("S3の方です", result);
     setCategoryImg(result);
-    console.log("svgです", categoryImg);
   }
 
   return (
     <ContentsWrapper>
       <h2>{articleIds.title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: categoryImg }}></div>
-      {/* <p>{articleIds.contents}</p> */}
+      <CategoryImageArticle>
+        <div dangerouslySetInnerHTML={{ __html: categoryImg }}></div>
+      </CategoryImageArticle>
       <div dangerouslySetInnerHTML={{ __html: articleIds.contents }}></div>
       <p>{articleIds.createdAt}</p>
     </ContentsWrapper>
