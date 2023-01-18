9391fdececec83ec08ae082df642c1ade424353e
yozakura <matchnokaori@gmail.com>
2023-01-18 23:01:32 +0900

    top page category icon fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import PageSEO from '@/components/PageSEO';
 import ContentsWrapper from '@/components/ContentsWrapper';
-import ArticleCard from '@/components/articles/ArticleCard';
+// import ArticleCard from '@/components/articles/ArticleCard';
 import CategoryIcon from '@/components/articles/CategoryIcon';
 
 export default function Home() {
@@ -9,7 +9,6 @@ export default function Home() {
       <PageSEO title="トップページ" />
       <ContentsWrapper>
         <CategoryIcon />
-        <ArticleCard />
       </ContentsWrapper>
     </>
   )
