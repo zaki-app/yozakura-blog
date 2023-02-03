90519993383ce15069ff8b1329944ef340039a0f
yozakura <matchnokaori@gmail.com>
2023-02-04 00:24:44 +0900

    syntax fix

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,6 +4,7 @@ export default function Profile () {
   return (
     <ContentsWrapper>
       <h1>プロフィールページです</h1>
+      <p>エンジニアに転職して１年目ですがマイペースに技術系のブログを更新していこうと思ってます！</p>
     </ContentsWrapper>
   )
 }
