b40db2a9b98c01e9b9c9e94b9d74ffc05552c220
yozakura <matchnokaori@gmail.com>
2023-03-04 21:24:32 +0900

    コンタクトからspreadsheetに保存

--- src/components/contact/Confirm.tsx
+++ src/components/contact/Confirm.tsx
@@ -3,7 +3,35 @@ import { contactValue } from "@/types/contactType";
 
 export default function ConfirmContact (props: any) {
   console.log(props)
+  console.log("url", process.env.NEXT_PUBLIC_CONTACT_GAS_URL)
   const value = props.formValue.input;
+  
+  const handleSubmit = async() => {
+    const params = props.formValue.input;
+    // gas用にparamを修正
+    const formBody = [];
+    for (let param in params) {
+      let encodeKey = encodeURI(param);
+      let encodeVal = encodeURIComponent(params[param]);
+      formBody.push(encodeKey + "=" + encodeVal);
+    }
+    const newBody = formBody.join("&");
+    console.log("新しいボディ", newBody);
+
+    // gasでスプレッドシートに保存
+    await fetch(process.env.NEXT_PUBLIC_CONTACT_GAS_URL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: newBody,
+    }).then(res => {
+      props.handleNext();
+    }).catch(err => {
+      alert("送信に失敗しました");
+      console.log("送信エラー", err)
+    })
+  }
 
   return (
     <>
@@ -17,7 +45,7 @@ export default function ConfirmContact (props: any) {
         戻って編集
       </Button>
       <Button
-        onClick={props.handleNext}
+        onClick={handleSubmit}
       >
         送信する
       </Button>
